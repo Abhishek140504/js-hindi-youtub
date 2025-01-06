03c1977d1e1ddd2e088693e2e15361854413460f
Abhishek140504 <abi20041128@gmail.com>
2025-01-06 12:37:46 +0000

    datatype

---
+++ 01_basics/05_summary.js
@@ -0,0 +1,14 @@
+// #primitive data types  ---all primitive types are call by value means it gives us the copy the code not reftrnce 
+
+//7 types of primitive data types- strings,numbers,boolean,null,undefined,symbol=makes value unique,bigint-bigvalues
+
+
+
+
+
+
+
+
+//non-primitive data types = refrence types 
+
+//arrays,objects,function
