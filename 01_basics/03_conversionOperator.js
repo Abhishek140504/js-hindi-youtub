e484c0133824155d8039f041b8019c5a0ac6362b
Abhishek140504 <abi20041128@gmail.com>
2025-01-06 12:12:47 +0000

    operations

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -1,11 +1,29 @@
-let score=33
-console.log(typeof score);
-let scoree="33"
-let valueInNumber =Number(scoree)
-console.log(typeof valueInNumber);
-// "33"=33
-// "33abc"=NaN
-// true=1; false =0
-let isLoggedIn="abhi"
-let booleanIsloggedIn=Boolean(isLoggedIn)
-console.log(typeof booleanIsloggedIn);
+// let score=33
+// console.log(typeof score);
+// let scoree="33"
+// let valueInNumber =Number(scoree)
+// console.log(typeof valueInNumber);
+// // "33"=33
+// // "33abc"=NaN
+// // true=1; false =0
+// let isLoggedIn="abhi"
+// let booleanIsloggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsloggedIn);
+
+
+// operations -------------
+
+// let value =3
+// let nagValue= -value
+// console.log(nagValue);
+
+// console.log("1"+2);// 12
+// console.log("1"+2+2);//122
+
+console.log(1+2+"2");//32
+
+
+
+
+
+
