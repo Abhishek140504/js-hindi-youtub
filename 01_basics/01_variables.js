4a959fec1824e45c08d62cb5db6df7ca5216d245
Abhishek140504 <abi20041128@gmail.com>
2025-01-06 11:27:06 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =144553
+let accountEmail ="abi22003@gmail.com"
+var accountPassword = "123456"
+accountCity="Ludhiana"
+
+// accountId =2 
+console.log(accountId)
+
+accountEmail="jhdjdbd@history.com"
+accountPassword="3783454"
+accountCity="jdv"
+let accountState;
+
+/*
+prfer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"// traet all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+let name="abhi"
+let age=20
+let isLoggedIn=false
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+// bollean=true/false 
+// null=> standalone value
+//undefined =>
+// symbol => unique 
+//objects
+
+console.log(typeof "abhi")
+console.log(typeof null)//object
+console.log(typeof undefined) // undefined
