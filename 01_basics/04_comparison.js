b7f7cf507e3b15fa36d7d1a4901730d7f436ce1b
Abhishek140504 <abi20041128@gmail.com>
2025-01-06 12:23:55 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,4 @@
+console.log(null>0)
+console.log(null==0)// == checks both sides are the same or not but it not convert null to 0
+console.log(null>=0) // converts null into number 0 so it is true
+console.log(undefined>=0)//false
