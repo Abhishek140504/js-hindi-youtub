86886233f6b0570a5e0e791ddefa77f7081c2b8f
Abhishek140504 <abi20041128@gmail.com>
2025-01-06 11:44:33 +0000

    conversion

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,11 @@
+let score=33
+console.log(typeof score);
+let scoree="33"
+let valueInNumber =Number(scoree)
+console.log(typeof valueInNumber);
+// "33"=33
+// "33abc"=NaN
+// true=1; false =0
+let isLoggedIn="abhi"
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsloggedIn);
