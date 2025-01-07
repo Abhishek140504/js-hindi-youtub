919f11fc120f62d3d0beb5e5f501fb5a08fe4e79
Abhishek140504 <abi20041128@gmail.com>
2025-01-07 07:18:52 +0000

    nums and maths

---
+++ 01_basics/06numbers.js
@@ -0,0 +1,23 @@
+// const balance=400;
+
+// const num =new Number(100)
+// console.log(num)
+
+// console.log(num.toString())
+// console.log(num.toFixed(6)) // 100.000000
+
+const num2=23.9822
+console.log(num2.toPrecision(3))//24.0
+console.log(num2.toPrecision(2))//24
+
+const hundreds =10000
+console.log(hundreds.toLocaleString())
+
+
+//maths topics its a library of javascript
+
+console.log(Math)
+
+console.log(Math.abs(-4))//output 4 abs convert - value to+ but not + to -
+
+console.log(Math.max(7,23,43,435,3324,435,5,4353,3,545))
